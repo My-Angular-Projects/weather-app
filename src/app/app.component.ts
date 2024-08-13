e87d3e34b3940f0f1ca5500c874202b50b7ca36c
Muso Verda <gearmobile@gmail.com>
2024-08-13 20:35:48 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wt-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'waether-app';
+}
